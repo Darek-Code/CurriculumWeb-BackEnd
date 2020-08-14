02a98f5d612ff6eec8cffbe4ab586f6fe8bea120
unknown <darek@writeme.com>
2020-08-14 10:31:26 +0200

    first commit

---
+++ notas.md
@@ -0,0 +1,16 @@
+/proyectos - vista con todos los proyectos
+
+GET /api/proyectos - recuperartodos los proyectos
+POST /api/proyectos - crear un nuevoproyecto
+PUT /api/proyectos/IDPROYECTO - editar un nuevo proyecto
+DELETE /api/proyectos/IDPROYECTO - borrar un proyecto
+
+- Instalar libreria CORS
+- Trabajar con un TOKEN
+    - Moment
+    - JWT
+- Subir la app a HEROKU
+- Recuperar proyectos por categoria
+- Swagger
+
+
---
+++ routes/api.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const moment = require('moment');
+const jwt = require('jwt-simple');
+
+const apiRouterProyectos = require('./api/proyectos');
+const middlewares = require('./middlewares');
+
+router.use('/proyectos', middlewares.checkToken, apiRouterProyectos);
+
+router.get('/token', (req, res) => {
+    let payload = {
+        usuario: 'Darek',
+        createdAt: moment().unix(),
+        expiredAt: moment().add(5, 'minutes').unix()
+    };
+    const token = jwt.encode(payload, process.env.SECRET_KEY);
+    console.log(token);
+    res.json({ token });
+
+});
+
+module.exports = router;
